c141bfe321ea1ed460e9d4757dac28186a058c42
ShlomiZi <33939764+ShlomiZi@users.noreply.github.com>
2021-04-16 00:24:45 +0300

    Update el8.sh

--- el8.sh
+++ el8.sh
@@ -4,12 +4,10 @@ cat << EOF > /etc/modprobe.d/nested.conf
 options kvm-intel nested=y
 EOF
 
-
 # jenkins-staging user only
 useradd jenkins-staging -G mock
 mkdir -p /home/jenkins-staging/.ssh
 chmod 700 /home/jenkins-staging/.ssh
-
 
 cat << EOF > /home/jenkins-staging/.ssh/authorized_keys
 ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCcaW1eHhuFfPKgDcSir/D2/qZlBwMSSXUXZi4F8SOt0C6WFggRcZB6kjk73GyzkZ879Wlr97WITAPXaaEFSNnaa0TSfTpuElqhOipR/IEM9KYDDfYIIoABBebhn6kpBBQ81gd3L4+8Lv6xse6YBu4/HhfILBbUN20DVUYd9vGUc2y0c9RasJjotdg7+1iUzbT/dqPG1OX/S4M/qIF6wcygnedHt2KtPE+QosCACNdtshGHwngO9H+wXv9e/37WFwU6dRMESCxrBAM+gxO8+0nLANW28GDr5EGYNs4gy5reyTKS8qqswHqK4h5bi7Ad1BSx29DUa8wEOUO/TV2eiUsz
